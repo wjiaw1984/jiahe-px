de918a7a8145e3692a34b2cbba6405df26783a76
wjw <wujiawen@e-future.com.cn>
2024-07-16 12:17:15 +0800

    项目创建

--- src/main/java/com/jiahe/px/common/base/request/RequestBaseVO.java
+++ src/main/java/com/jiahe/px/common/base/request/RequestBaseVO.java
@@ -1,6 +1,6 @@
 package com.jiahe.px.common.base.request;
 
-import com.alibaba.fastjson.annotation.JSONField;
+import com.alibaba.fastjson2.annotation.JSONField;
 import lombok.Data;
 
 import java.io.Serializable;
